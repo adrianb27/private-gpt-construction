3c656a0a643401b5ec7f74f7dc66174cc77c2780
adrianb27 <info.adrianb@gmail.com>
2025-07-08 16:21:51 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Installing dependencies..."
-pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt --no-cache-dir
 
 echo "Ingesting sample documents..."
 python ingest/ingest_documents.py
 
 echo "Launching Streamlit UI..."
-streamlit run app/main.py
+python3 -m streamlit run app/main.py --server.port=8501 --server.address=0.0.0.0
